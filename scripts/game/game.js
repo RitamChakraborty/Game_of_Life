7497530884566cb88004456c253c4341c3dc0ac6
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-07-25 17:45:54 +0530

    Change cell status on click

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -1,3 +1,11 @@
 let arr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
-export {arr};
+function updateCell(i, j) {
+    if (arr[i][j] === 0) {
+        arr[i][j] = 1;
+    } else if (arr[i][j] === 1) {
+        arr[i][j] = 0;
+    }
+}
+
+export {arr, updateCell};
--- scripts/view/board.js
+++ scripts/view/board.js
@@ -1,5 +1,6 @@
 import {ALIVE, DEAD} from "../data/constant.js";
 import {getSize} from "../util/size.js";
+import {updateCell} from "../game/game.js";
 
 const container = document.getElementsByClassName("container")[0];
 
@@ -17,17 +18,20 @@ function drawBoard(arr) {
 
         for (let j = 0; j < arr[i].length; ++j) {
             const td = document.createElement("td");
-
-            // Set background color based on cell status
-            if (arr[i][j] ===  0) {
-                td.style.backgroundColor = DEAD;
-            } else if (arr[i][j] === 1) {
-                td.style.backgroundColor = ALIVE;
-            }
+            td.id = `${i}_${j}`;
 
             // Set cell height and width
             td.style.height = `${size.height}px`;
             td.style.width = `${size.width}px`;
+
+            td.onclick = cellClick;
+
+            // Set background color based on cell status
+            if (arr[i][j] === 0) {
+                td.style.backgroundColor = DEAD;
+            } else if (arr[i][j] === 1) {
+                td.style.backgroundColor = ALIVE;
+            }
 
             tr.appendChild(td);
         }
@@ -38,6 +42,27 @@ function drawBoard(arr) {
     // Add table as the only child of the container
     container.firstChild?.remove();
     container.appendChild(table);
+}
+
+function cellClick(event) {
+    const id = event.target.id;
+    const values = id.split("_");
+    const i = parseInt(values[0]);
+    const j = parseInt(values[1]);
+
+    changeCellColor(id);
+    updateCell(i, j);
+}
+
+function changeCellColor(id) {
+    const element = document.getElementById(id);
+    let backgroundColor = element.style.backgroundColor;
+
+    if (backgroundColor === ALIVE) {
+        element.style.backgroundColor = DEAD;
+    } else if (backgroundColor === DEAD) {
+        element.style.backgroundColor = ALIVE;
+    }
 }
 
 export {drawBoard};
